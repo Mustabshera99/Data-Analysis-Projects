1a1279474bef3067d81be57ef590ae9db5e56a20
Mustabshera99 <150914262+Mustabshera99@users.noreply.github.com>
2023-11-23 13:27:41 +0500

    Add files via upload
    Movies recommended to Customers

---
+++ R6 final_recommdation.sql
@@ -0,0 +1,11 @@
+select 
+DISTINCT 
+cus.customer_id,
+cus.first_name,
+cus.category,
+ml.film_title 
+ 
+from customer_category cus
+left join top_recomanded_movies ml 
+on cus.customer_id = ml.customer_id and cus.category = ml.category
+WHERE ml.film_title IS NOT NULL
